353e377aaa4718ffcd4b3f4b374eb5401a41b3a2
dingzhenyue <dingzhenyue@yiche.com>
2021-04-22 18:44:46 +0800

    每一项宽度计算

--- index.js
+++ index.js
@@ -2,17 +2,30 @@
  * @Description: 
  * @Author: dzy
  * @Date: 2021-04-22 11:08:28
- * @LastEditTime: 2021-04-22 18:10:44
+ * @LastEditTime: 2021-04-22 18:44:06
  * @LastEditors: dzy
  * @Reference: 
  */
 let intervalIn = null;
 let intervalOut = null;
-const childDefaultWidth = 100;
-const parentWidth = 500;
 // 定时动画间隔10ms
 const TIME = 10;
 const ONCE_MOVE_PX = 1;
+// 父级宽度
+let Parent_Width = null;
+// 每一项的默认宽度
+let Child_Default_Width = null;
+/**
+ * 初始化每一项的宽度（平分）
+ */
+let initChildDomWidth = function () {
+    let parentDom =  document.querySelector('.parent');
+    Parent_Width = parentDom.offsetWidth;
+    Child_Default_Width = Parent_Width / 5;
+    Array.from(document.querySelectorAll('.child-item')).map(item => {
+        item.style.width = Child_Default_Width + 'px';
+    });
+}
 /**
  * 鼠标移入，宽度变成2倍
  * @param {*} div 
@@ -26,11 +39,11 @@ let widthToDouble = function (div) {
     }
     intervalIn = setInterval(function () {
         let otherDiv = Array.from(document.querySelectorAll('.child-item')).filter(item => item !== div);
-        if (2 * childDefaultWidth === div.offsetWidth) {
+        if (2 * Child_Default_Width === div.offsetWidth) {
             clearInterval(intervalIn);
         } else {
             div.style.width = div.offsetWidth + ONCE_MOVE_PX + "px";
-            let otherChildWidth = (parentWidth - div.offsetWidth) / otherDiv.length + 'px';
+            let otherChildWidth = (Parent_Width - div.offsetWidth) / otherDiv.length + 'px';
             otherDiv.map(item => {
                 item.style.width = otherChildWidth;
             });
@@ -50,15 +63,18 @@ let widthToHalf = function (div) {
     }
     intervalOut = setInterval(function () {
         let otherDiv = Array.from(document.querySelectorAll('.child-item')).filter(item => item !== div);
-        if (childDefaultWidth === div.offsetWidth) {
+        if (Child_Default_Width === div.offsetWidth) {
             clearInterval(intervalOut);
         } else {
             div.style.width = div.offsetWidth - ONCE_MOVE_PX + "px";
-            let otherChildWidth = (parentWidth - div.offsetWidth) / otherDiv.length + 'px';
+            let otherChildWidth = (Parent_Width - div.offsetWidth) / otherDiv.length + 'px';
             otherDiv.map(item => {
                 item.style.width = otherChildWidth;
             });
         }
     }, TIME);
 }
+
+initChildDomWidth();
+
 
