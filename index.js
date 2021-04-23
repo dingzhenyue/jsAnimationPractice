bc760153bd3493bdada50914f7fa220865f3264e
dingzhenyue <dingzhenyue@yiche.com>
2021-04-23 09:09:24 +0800

    部分格式修改

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: dzy
  * @Date: 2021-04-22 11:08:28
- * @LastEditTime: 2021-04-22 18:44:06
+ * @LastEditTime: 2021-04-23 09:07:47
  * @LastEditors: dzy
  * @Reference: 
  */
@@ -10,11 +10,14 @@ let intervalIn = null;
 let intervalOut = null;
 // 定时动画间隔10ms
 const TIME = 10;
+// 定义移动步长1px
 const ONCE_MOVE_PX = 1;
 // 父级宽度
 let Parent_Width = null;
 // 每一项的默认宽度
 let Child_Default_Width = null;
+
+
 /**
  * 初始化每一项的宽度（平分）
  */
@@ -26,6 +29,7 @@ let initChildDomWidth = function () {
         item.style.width = Child_Default_Width + 'px';
     });
 }
+
 /**
  * 鼠标移入，宽度变成2倍
  * @param {*} div 
@@ -50,6 +54,7 @@ let widthToDouble = function (div) {
         }
     }, TIME);
 }
+
 /**
  * 鼠标移出，宽度恢复默认
  * @param {*} div 
